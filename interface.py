3c22e4867c6c892fc039191390f5f551ef6eac38
Nicholas Tanugroho <117884155+Someonelse2309@users.noreply.github.com>
2025-06-03 00:56:42 +0700

    first commit

---
+++ interface.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import cv2
+import numpy as np
+from program import replacingColor
+
+st.session_state.colorReplace = replacingColor()
+
+if "status" not in st.session_state:
+    st.session_state.status = False
+
+uploaded_file = st.file_uploader("Upload Gambar Anda", type=["jpg", "jpeg", "png"], accept_multiple_files=False)
+
+if uploaded_file is not None:
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, 1)
+    st.image(img, channels='BGR', caption="Gambar Sebelum")
+else:
+    st.write("Belum ada gambar di-upload.")
+
+tab1, tab2 = st.tabs(["Pengaturan Warna", "Pengaturan Threshold"])
+
+with tab1:
+    col1, col2 = st.columns(2)
+    with col1:
+        colorAwal = st.color_picker("Warna yang ingin diganti",
+                                    help="Warna yang ingin diganti dari gambar.")
+
+    with col2:
+        colorAkhir = st.color_picker("Ganti dengan warna ini",
+                                     help="Warna dari gambar akan diganti dengan warna ini.")
+
+
+
+with tab2:
+    col1T, col2T, col3T = st.columns(3)
+    hTol, sTol, vTol = st.session_state.colorReplace.getThreshold()
+    with col1T:
+        hTolInp = st.number_input(
+            label="Toleransi H",
+            max_value=255,
+            min_value=0,
+            value=hTol,
+            placeholder="Masukan Toleransi H Ada",
+            help="Seberapa banyak variasi \'jenis\' warna yang masih Anda anggap sama, misalnya seberapa jauh sebuah warna bisa bergeser dari \'merah murni\' tapi tetap dianggap merah (bukan oranye atau pink).",
+        )
+
+    with col2T:
+        sTolInp = st.number_input(
+            label="Toleransi S",
+            max_value=255,
+            min_value=0,
+            value=sTol,
+            placeholder="Masukan Toleransi S Ada",
+            help="Seberapa pudar atau seberapa mencolok sebuah warna yang masih Anda terima, misalnya apakah merah yang sedikit pudar masih dianggap sama dengan merah yang sangat pekat."
+        )
+
+    with col3T:
+        sTolInp = st.number_input(
+            label="Toleransi V",
+            max_value=255,
+            min_value=0,
+            value=vTol,
+            placeholder="Masukan Toleransi V Ada",
+            help="Seberapa terang atau gelap sebuah warna yang masih Anda anggap cocok, misalnya apakah merah yang agak gelap masih sama dengan merah yang sangat terang di bawah sinar matahari."
+        )
+
+
+if st.button("Ubah Warna"):
+    st.session_state.colorReplace.changeThreshold(hTolInp,sTolInp,sTolInp)
+    st.session_state.colorReplace.inputImage(img, colorAwal,colorAkhir)
+    st.session_state.status = True
+
+if st.session_state.get("status", False):
+    col1Result, col2Result = st.columns(2)
+    with col1Result:
+        st.image(st.session_state.colorReplace.getMasking(), channels='GRAY', caption="Hasil Masking")
+    with col2Result:
+        st.image(st.session_state.colorReplace.getResult(), channels='BGR', caption="Hasil Akhir")
+else:
+    pass
---
+++ program.py
@@ -0,0 +1,82 @@
+import cv2
+import numpy as np
+
+class replacingColor():
+
+    def __init__(self, hTol = 80, sTol = 80, vTol = 80):
+        self.hTol = hTol
+        self.sTol = sTol
+        self.vTol = vTol
+
+    def changeThreshold(self, hTol, sTol, vTol):
+        self.hTol = hTol
+        self.sTol = sTol
+        self.vTol = vTol
+    
+    def getThreshold(self):
+        return self.hTol, self.sTol, self.vTol
+
+    def inputImage(self, img, colorBefore, colorAfter):
+        self.img = img
+        self.hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
+
+        self.before = self.convertToBGR(colorBefore)
+        self.after =  self.convertToBGR(colorAfter)
+
+        self.processImages()
+    
+    def convertToBGR(self, hex):
+        RGBCol = [int(hex[i:i+2], 16) for i in (1, 3, 5)]
+        BGRCol = RGBCol[::-1]
+        return  np.uint8([[BGRCol]])
+    
+    def convertToHSV(self, BGRCol):
+        HSVCol = cv2.cvtColor(BGRCol, cv2.COLOR_BGR2HSV)
+        print(BGRCol, HSVCol)
+        return HSVCol[0][0]
+
+    
+    def processImages(self):
+        hB, sB, vB = self.convertToHSV(self.before) 
+        # hA, sA, vA = self.convertToHSV(self.after) 
+        batasAtas = np.array([
+            min(int(hB) + self.hTol, 179),
+            min(int(sB) + self.sTol, 255),
+            min(int(vB) + self.vTol, 255)
+        ])
+        batasBawah = np.array([
+            max(int(hB) - self.hTol, 0),
+            max(int(sB) - self.sTol, 0),
+            max(int(vB) - self.vTol, 0)
+        ])
+
+
+        print("Lower bound:", batasBawah)
+        print("Upper bound:", batasAtas)
+
+
+        self.masking = cv2.inRange(self.hsv, batasBawah, batasAtas)
+        if np.count_nonzero(self.masking) == 0:
+            print("Warning: Mask kosong, threshold tidak cocok!")
+
+        warna_ganti = tuple(int(c) for c in self.after[0][0])
+        
+        self.result = self.img.copy()
+        self.result[self.masking > 0] =  warna_ganti
+
+    def getResult(self):
+        return self.result
+    
+    def getMasking(self):
+        return self.masking
+
+    
+if __name__ == "__main__":
+    img = cv2.imread('download.png')
+    hexBefore = "#0000ff"
+    hexAfter = "#F6Ac48"
+    temp = replacingColor(img,hexBefore,hexAfter)
+    finalImg = temp.getResult()
+    cv2.imshow("nama",finalImg)
+    cv2.waitKey(0)
+
